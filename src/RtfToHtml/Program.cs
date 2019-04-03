3d2c1dd457d7d910cee3c917f0eac4800059e546
Worameth Semapat <31945018+chen2584@users.noreply.github.com>
2019-04-03 16:39:47 +0700

    first commit

---
+++ src/RtfToHtml/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using RtfPipe;
+
+namespace RtfToHtml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add a reference to the NuGet package System.Text.Encoding.CodePages for .Net core only
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var location = Assembly.GetExecutingAssembly().Location;
+            var contentRoot = Path.GetDirectoryName(location);
+            var input = Path.Combine(contentRoot, "input.rtf");
+            var output = Path.Combine(contentRoot, "output.html");
+
+            var html = Rtf.ToHtml(File.ReadAllText(input));
+            File.WriteAllText(output, html);
+
+            Console.WriteLine($"Done. Output file: {output}");
+        }
+    }
+}
